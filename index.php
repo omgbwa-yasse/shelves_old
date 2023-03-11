dbc2ead200436a27c63ff0911835e8a629714f7f
omgbwa yasse <omgbwayasse@gmail.com>
2023-03-11 21:01:38 +0100

    Personnalisation test 2

--- index.php
+++ index.php
@@ -4,10 +4,50 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/bootstrap.min.css" >
+    <link rel="stylesheet" href="style/bootstrap.css" >
     <title>Administrator</title>
 </head>
 <body>
+    <script src="style/bootstrap.bundle.min.js"></script>
+    <script src="style/popper.min.js"></script>
+    <script src="style/bootstrap.min.js"></script>
 
+<img alt="logo" src="#" width="100%" height="100px" />
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor02">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Home
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
     
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-sm-2" type="search" placeholder="Recherche">
+        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Rechercher</button>
+        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Avancée</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
 </body>
 </html>
