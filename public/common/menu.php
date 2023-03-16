bc79a1306d976fc2650931f57b078fdcf2855ecd
David <davidnjandjeu@gmail.com>
2023-03-16 14:54:31 +0100

    usager
    usagera

---
+++ public/common/menu.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="menu">
+        <ul>
+            <li>recherche</li>
+            <li>message</li>
+            <li>Versement</li>
+            <li>Chariot</li>
+            <li>Parametre</li>
+        </ul>
+    </div>
+</body>
+</html>
--- public/index.php
+++ public/index.php
@@ -1,16 +1,15 @@
-<?
-
+<?php
+include('common/topbar.php');
 
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
+<section class="main">
+    <?php 
+       include('common/menu.php');
+    ?>
+    <div class="container">
+       <p>felementf</p>
+    </div>    
+</section>
+
 </body>
 </html>
