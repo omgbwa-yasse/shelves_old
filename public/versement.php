e338f4059ae7eb13dc75426ef9564bd1d20f789b
David <davidnjandjeu@gmail.com>
2023-03-18 02:45:34 +0100

    creation
    creation des autres fichier

---
+++ public/versement.php
@@ -0,0 +1,16 @@
+<?php
+include('common/topbar.php');
+
+?>
+<section class="main">
+    <?php 
+       include('common/menu.php');
+    ?>
+    <div class="container">
+
+    </div> 
+    
+</section>
+
+</body>
+</html>
