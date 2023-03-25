8867bb5eadefab374724a3eb93cfe5bb6e7d99c7
omgbwa yasse <omgbwayasse@gmail.com>
2023-03-25 11:03:10 +0100

    Methodes CSS

--- controller/config.php
+++ controller/config.php
@@ -3,7 +3,7 @@
 $host = "localhost";
 $dbname = "dbms";
 $username = "root";
-$password = "root";
+$password = "";
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
