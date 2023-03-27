aeb5af92a30650361dbf2ed9d98fd707c141daaa
David <davidnjandjeu@gmail.com>
2023-03-28 00:22:35 +0100

    les liens sont actif

--- template/template.php
+++ template/template.php
@@ -22,13 +22,13 @@ if (isset($_GET['q'])){
 </header>
 <nav>
     <ul>
-        <li><a href ="../shelves/index.php?q=repertoire">Repertoire</a></li>
-        <li><a href ="../shelves/index.php?q=versement">Versement</a></li>
-        <li><a href ="../shelves/index.php?q=demande">Demande</a></li>
-        <li><a href ="../shelves/index.php?q=deposit">Dépôt</a></li>
-        <li><a href ="../shelves/index.php?q=dolly">Chariot</a></li>
-        <li><a href ="../shelves/index.php?q=outilsGestion">Outils de gestion</a></li>
-        <li><a href ="../shelves/index.php?q=parametre"> Parametre</a></li>
+        <li class="<?php if ($q=="repertoire") {echo "active"; } else  {echo "";}?>"><a href ="../shelves/index.php?q=repertoire">Repertoire</a></li>
+        <li class="<?php if ($q=="versement") {echo "active"; } else  {echo "";}?>"><a href ="../shelves/index.php?q=versement">Versement</a></li>
+        <li class="<?php if ($q=="demande") {echo "active"; } else  {echo "";}?>"><a href ="../shelves/index.php?q=demande">Demande</a></li>
+        <li class="<?php if ($q=="deposit") {echo "active"; } else  {echo "";}?>"><a href ="../shelves/index.php?q=deposit">Dépôt</a></li>
+        <li class="<?php if ($q=="dolly") {echo "active"; } else  {echo "";}?>"><a href ="../shelves/index.php?q=dolly">Chariot</a></li>
+        <li class="<?php if ($q=="outilsGestion") {echo "active"; } else  {echo "";}?>"><a href ="../shelves/index.php?q=outilsGestion">Outils de gestion</a></li>
+        <li class="<?php if ($q=="parametre") {echo "active"; } else  {echo "";}?>"><a href ="../shelves/index.php?q=parametre"> Parametre</a></li>
     </ul>
 </nav>
 
