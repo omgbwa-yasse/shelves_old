f38e5e2d98ceb66101ae67225bc6855ce1a124dc
omgbwa yasse <omgbwayasse@gmail.com>
2023-03-28 13:07:24 +0100

    Mise à jour des outils de gestion

--- views/outilsGestion.views.php
+++ views/outilsGestion.views.php
@@ -1,12 +1,32 @@
 <?php 
 include "template/sous_menu.inc.php";
-echo '<ul>
-        <li><a href="/'.'../shelves/'.'index?q=outilsGestion&categ=planClassement"/> Plan de classement </a></li>
-        <li><a href="/'.'../shelves/'.'index?q=outilsGestion&categ=retention"/> Referentiel de conservation </a></li>
-        <li><a href="/'.'../shelves/'.'index?q=outilsGestion&categ=communicability"/> Communicabilité </a></li>
-        <li><a href="/'.'../shelves/'.'index?q=outilsGestion&categ=accessClassification"/> Classification accès </a></li>
-        <li><a href="/'.'../shelves/'.'index?q=outilsGestion&categ=organization"/> Organigramme </a></li>
-        </ul>';
+?>
+<p>Plan de classement</p>
+        <ul>
+                <li><a href="../shelves/index.php?q=outilsGestion&categ=planClassement&sub=allClass"> Plan de classement </a></li>
+                <li><a href="../shelves/index.php?q=outilsGestion&categ=planClassement&sub=addClasse"> Ajouter une classe </a></li>
+        </ul>
+<p>Reférentiel de conservation</p> 
+        <ul>       
+                <li><a href="../shelves/index.php?q=outilsGestion&categ=retention&sub=allDua"> Référentiel de conservation</a></li>     
+                <li><a href="../shelves/index.php?q=outilsGestion&categ=retention&sub=addDuaRule"> Ajouter une règles </a></li>
+        </ul>
+<p>Règles de communicabilité</p>
+        <ul>    
+                <li><a href="../shelves/index.php?q=outilsGestion&categ=communicability&sub=allCommunicability"> Règle communicabilité </a></li>
+                <li><a href="../shelves/index.php?q=outilsGestion&categ=communicability&sub=addCommunicabilityRule"> Ajouter une règle </a></li>
+        </ul>
+<p>Classe d'accès</p>  
+        <ul>      
+                <li><a href="../shelves/index.php?q=outilsGestion&categ=accessClassification&sub=allAccess"> Classification pour l'accès </a></li>      
+                <li><a href="../shelves/index.php?q=outilsGestion&categ=accessClassification&sub=addAccessRule"> Ajouter une règle d'accès </a></li>
+        </ul>
+<p>Organigramme</p>
+        <ul> 
+                <li><a href="../shelves/index.php?q=outilsGestion&categ=organization&sub=allOrganization"> Organigramme </a></li>
+                <li><a href="../shelves/index.php?q=outilsGestion&categ=organization&sub=addOrganization"> Ajouter un service </a></li>
+        </ul>
 
-include "template/container.inc.php";
+<?php
+        include "template/container.inc.php";
 ?>
