268e88b606762f1cd4c1f0513a48aeb60e7d9e80
omgbwa yasse <omgbwayasse@gmail.com>
2023-03-28 13:22:35 +0100

    Modification du fichier Dolly

--- views/dolly.views.php
+++ views/dolly.views.php
@@ -1,11 +1,31 @@
 <?php 
 include "template/sous_menu.inc.php";
-    echo '<ul>
-    <li><a href="/'.'../shelves/'.'index?q=dolly&categ=all"/> Tous les chariots</a></li>
-    <li><a href="/'.'../shelves/'.'index?q=dolly&categ=records"/> Chariot description </a></li>
-    <li><a href="/'.'../shelves/'.'index?q=dolly&categ=transfert"/> Chariot versement </a></li>
-    <li><a href="/'.'../shelves/'.'index?q=dolly&categ=container"/> Chariot contenant </a></li>
-    <li><a href="/'.'../shelves/'.'index?q=dolly&categ=loan"/> Chariot demandes </a></li>
-    </ul>';
+?>
+
+    <p>Chariot des enregistrements</p>
+    <ul>
+    <li><a href="../shelves/index?q=dolly&categ=records"> Tous les chariots </a></li>
+    <li><a href="../shelves/index?q=dolly&categ=records"> Ajouter un chariot </a></li>
+    </ul>
+
+    <p>Chariot des versements</p>
+    <ul>
+    <li><a href="../shelves/index?q=dolly&categ=transfert"> Tous les chariots </a></li>
+    <li><a href="../shelves/index?q=dolly&categ=transfert"> Ajouter un chariot </a></li>
+    </ul>
+
+    <p>Chariot de contenants</p>
+    <ul>
+    <li><a href="../shelves/index?q=dolly&categ=container"> Tous les chariots </a></li>
+    <li><a href="../shelves/index?q=dolly&categ=container"> Ajouter un chariot </a></li>
+    </ul>
+
+    <p>Chariot de demandes</p>
+    <ul>
+    <li><a href="../shelves/index?q=dolly&categ=loan"> Tous les chariots </a></li>
+    <li><a href="../shelves/index?q=dolly&categ=loan"> Ajouter un chariot </a></li>
+    </ul>
+
+<?php
 include "template/container.inc.php";
 ?>
