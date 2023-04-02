a6e3d7231bafeb3b625353a9a7d4146caa14b489
omgbwa yasse <omgbwayasse@gmail.com>
2023-04-02 08:14:03 +0100

    Boucle enregistrement des mots clés réussit

--- views/inventory/createRecords.inc.php
+++ views/inventory/createRecords.inc.php
@@ -72,7 +72,7 @@ $allContainer->execute();
 } ?>
 </select>
 </td></tr>
-<tr> <td> Mots-cléfs : </td> <td> <input type="text" name="keyword" size="70"> </td>  </tr>
+<tr> <td> Mots-cléfs : </td> <td> <input type="text" name="keywords" size="70"> </td>  </tr>
 <tr> <td> <input type="submit" size="30"> </td>  <td> <input type="reset" size="30"></td></tr>
 </table>
 </form>
