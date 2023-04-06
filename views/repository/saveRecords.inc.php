7d1cb01eed4325f479c115b870fbec742920d464
omgbwa yasse <omgbwayasse@gmail.com>
2023-04-06 17:57:08 +0100

    Traitement en cours ...

--- views/repository/saveRecords.inc.php
+++ views/repository/saveRecords.inc.php
@@ -2,20 +2,20 @@
 $cnx = new PDO("mysql:host=localhost;dbname=dbms", "root", "");
 $cnx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-echo '<hr> Liste des données envoyées par POST <br> ';
-echo $_POST['nui']= htmlspecialchars ($_POST['nui']);
-echo $_POST['title']= htmlspecialchars ($_POST['title']);
-echo $_POST['date_start'] = htmlspecialchars ($_POST['date_start']);
-echo $_POST['date_end']= htmlspecialchars ($_POST['date_end']);
-echo $_POST['observation'] = htmlspecialchars($_POST['observation']) ;
-echo $_POST['code_title'] = htmlspecialchars ($_POST['code_title']);
-echo $_POST['support']= htmlspecialchars ($_POST['support']);
-echo $_POST['container']= htmlspecialchars ($_POST['container']);
-echo $_POST['statut']= htmlspecialchars ($_POST['statut']);
-echo $_POST['keywords']= htmlspecialchars ($_POST['keywords']);
+$_POST['nui']= htmlspecialchars ($_POST['nui']);
+$_POST['title']= htmlspecialchars ($_POST['title']);
+$_POST['date_start'] = htmlspecialchars ($_POST['date_start']);
+$_POST['date_end']= htmlspecialchars ($_POST['date_end']);
+$_POST['observation'] = htmlspecialchars($_POST['observation']) ;
+$_POST['code_title'] = htmlspecialchars ($_POST['code_title']);
+$_POST['support']= htmlspecialchars ($_POST['support']);
+$_POST['container']= htmlspecialchars ($_POST['container']);
+$_POST['statut']= htmlspecialchars ($_POST['statut']);
+$_POST['keywords']= htmlspecialchars ($_POST['keywords']);
 $supportTitle = $_POST['support'] ;
 
-echo '<hr> Recupartion des Id des données envoyées  <br>  ';
+// echo '<hr> Recupartion des Id des données envoyées  <br>  ';
+
 $classe = "";
 $classeId = "SELECT classification_id FROM classification WHERE classification_code_title = '".$_POST['code_title']."' " ;
 $classeId=$cnx->prepare($classeId);
@@ -57,6 +57,7 @@ $rqtSave = "INSERT INTO records (id_records,records_nui, records_title, records_
                                 records_support_id, records_link_id, container_id, classification_id ) 
             values ('".NULL."','".$_POST['nui']."','".$_POST['title']."','".$_POST['date_start']."','".$_POST['date_end']."',
             '".$_POST['observation']."','".$statut."','".$support."','".NULL."','".$container."','".$classe."' )";
+
 $rqtSave = $cnx->prepare($rqtSave);
 if($rqtSave ->execute()){ 
 
