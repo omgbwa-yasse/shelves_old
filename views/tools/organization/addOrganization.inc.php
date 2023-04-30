32d1e7261584b6bed95b0cc2a06628d95be1177b
omgbwa yasse <omgbwayasse@gmail.com>
2023-04-30 20:19:19 +0100

    Organization creation start

---
+++ views/tools/organization/addOrganization.inc.php
@@ -0,0 +1,9 @@
+
+
+
+<form method="POST" action="index.php?q=outilsGestion&categ=organization&sub=saveOrganization">
+    Unite : <input type="texte" name="unite"><br/>
+    Parent :<input type="texte" name="unite_parent"> <br/>
+    Observation :<input type="texte" name="observation"> <br/>
+    <input type="submit" name="enregister">
+</form>
