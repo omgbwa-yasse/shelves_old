19c6db79ef2f322b940b3cc6f32a7ff83c4b9750
David <davidnjandjeu@gmail.com>
2023-04-30 12:56:28 +0100

    Update addClasse.inc.php

--- views/tools/planClassement/addClasse.inc.php
+++ views/tools/planClassement/addClasse.inc.php
@@ -1,26 +1,31 @@
 <?php
 require 'models/connexion.class.php';
-$allclassification = new classification();
 
+$cnx = new PDO("mysql:host=localhost;dbname=dbms;charset=utf8","root", "");
 ?>
  <?php 
-  $tiltle=$allclassification->getAllClassTitle();
-  $tiltle=$allclassification->setFetchMode(PDO::FETCH_ASSOC);
-
-
-
-     ?>
+          $sql = "SELECT *
+          FROM dbms.classification_type";
+          $allClasse = $cnx->prepare($sql);
+          $allClasse->execute();
+          $result = $allClasse->setFetchMode(PDO::FETCH_ASSOC);?>
 <form method="post" action="">
    
     Code de classification : <input type="text" name="classification_code"><br/>
     Titre de classification :<input type="text" name="classification_type_title"><br/>
     type de classification : <select name="classification_type" id="">
-   <?php foreach($tiltle->fetchAll() as $row) {
+   <?php foreach($allClasse->fetchAll() as $row) {
             echo "<option>" . $row["classification_type_title"] . "</option>";}?>
     </select><br>
     id Parent : <select name="classification_parent_id" >
         <?php
-          foreach($tiltle->fetchAll() as $row) {
+        $sql = "SELECT *
+        FROM dbms.classification_type
+        JOIN dbms.classification ON classification_type.classification_type_id = classification.classification_type_id";
+        $allClasse = $cnx->prepare($sql);
+        $allClasse->execute();
+        $result = $allClasse->setFetchMode(PDO::FETCH_ASSOC);
+          foreach($allClasse->fetchAll() as $row) {
             echo "<option>" . $row["classification_title"] . "</option>";}?>
     </select><br/>
     Observation : <input type="textarea" name="classification_observation"><br/>
