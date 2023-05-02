0dde22d3a0b5f4a992971d1365d84c5db088be10
omgbwa yasse <omgbwayasse@gmail.com>
2023-05-02 07:24:42 +0100

    Séparation des classes session et connexion

--- models/connexion.class.php
+++ models/connexion.class.php
@@ -12,24 +12,4 @@
         if(self::$cnx===null) { self::setCnx(); }
         return self::$cnx;
     }
-    }
-
-  class session extends connexion{
-  public function login(){
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $stmt = $this->getCnx()->prepare('SELECT * FROM user WHERE user_name = :username AND user_password = :password');
-    $stmt->execute(array(':username' => $username, ':password' => $password));
-    if ($stmt->rowCount() == 1) {
-        $_SESSION['logged_in'] = true;
-        header('Location: home.php');
-        exit;
-    } else {
-        $error = 'Invalid username or password';
-        return $error;
-    }
-  }
-  
-  }
-
-?>
+}?>
