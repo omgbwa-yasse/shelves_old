cc1a6e6eced35e4762b1c2d4f6a10d5af6371220
omgbwa yasse <omgbwayasse@gmail.com>
2023-05-02 22:36:28 +0100

    Nettoyage

--- views/repository/lastRecords.inc.php
+++ views/repository/lastRecords.inc.php
@@ -4,11 +4,10 @@ include_once 'models/repository/records.class.php';
 include_once 'models/repository/recordsManager.class.php';
 require_once 'views/repository/display.inc.php';
 ?>
-<h2>Pour plus de résultat 
-    <b>
-        <a href ="../shelves/index.php?q=repository&categ=search&sub=allrecords">Tous les enregistrements</a>
-    </b>
-</h2>
+<div style="border-radius:5px;margin-bottom:30px;padding:0.5em;border:solid 2px red;font-size:20px;font-weight:bold; width:900px;">
+<a href ="../shelves/index.php?q=repository&categ=search&sub=allrecords">Tous les enregistrements
+</a></div>
+
 <?php
 $lastRecords = new recordsManager();
 $list = $lastRecords->MgGetLastRecords();
