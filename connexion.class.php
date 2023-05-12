e53378c029f68d6df8dbfdf9b1b701a9ce510175
omgbwa yasse <omgbwayasse@gmail.com>
2023-05-12 10:56:45 +0100

    Développement des onglets DollyRecord

---
+++ connexion.class.php
@@ -0,0 +1,15 @@
+<?php
+ abstract class connexion{
+    private static $cnx;
+
+    private static function setCnx(){
+        self::$cnx = new PDO("mysql:host=localhost;dbname=dbms;charset=utf8","root", "");
+            self::$cnx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    }
+
+
+    protected function getCnx(){
+        if(self::$cnx===null) { self::setCnx(); }
+        return self::$cnx;
+    }
+}?>
