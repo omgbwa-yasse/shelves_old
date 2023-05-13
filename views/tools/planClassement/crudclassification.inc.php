70dc5a590c48b64ab74bd859222c0f6407f48997
David <davidnjandjeu@gmail.com>
2023-05-13 03:20:27 +0100

    Update crudclassification.inc.php

--- views/tools/planClassement/crudclassification.inc.php
+++ views/tools/planClassement/crudclassification.inc.php
@@ -3,21 +3,20 @@ require_once  '../../../../models/tools/classification/classe.class.php';
 
 $classification = new activityClassesManager() ;
 
-// vérification si l'ID de la classification est défini dans l'URL
+
 if (isset($_GET['id'])) {
-    // récupération de l'ID de la classification
+  
     $id = $_GET['id'];
 
-    // vérification si l'action est définie dans l'URL
     if (isset($_GET['f'])) {
-        // récupération de l'action
+    
         $action = $_GET['f'];
 
         if ($action === 'D') {
-            // suppression de la classification
+      
             $classification->deleteClassification($id);
         } elseif ($action === 'U') {
-            // récupération des données du formulaire
+
             $data = [
                 'classification_code' => $_POST['code'],
                 'classification_title' => $_POST['title'],
@@ -27,7 +26,6 @@ if (isset($_GET['id'])) {
                 'classification_observation' => $_POST['observation']
             ];
 
-            // mise à jour de la classification
             $classification->updateClassification($id, $data);
         }
     }
