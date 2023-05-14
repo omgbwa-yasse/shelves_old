3e56b552ea6413deab1c2005e786042269394652
omgbwa yasse <omgbwayasse@gmail.com>
2023-05-14 03:57:09 +0100

    Connexion utilisateur

--- views/connexion/connexionForm.views.php
+++ views/connexion/connexionForm.views.php
@@ -1,13 +1,32 @@
+<html>
+<head>
+        <title>My Shelves</title>
+        <link href="template/css/style.css" rel="stylesheet">
+</head>
+        <body>
+<center>
+<div class="connexion">
 <?php
 
-
-        echo "Formulaire de connxion";
+        echo "<h1>My Shelves</h1>";
 ?>
+<div>
 <form action='index.php?q=session&categ=user&sub=accessControl' method='post'>
-        <label for='pseudo'>Pseudo:</label>
-        <input type='text' id='pseudo' name='pseudo' required>
-        <label for='password'>Password:</label>
-        <input type='password' id='password' name='password' required>
-        <button type='submit'>Log in</button>
+        <table>
+        <tr>
+                <td><label for='pseudo'>Pseudo:</label></td>
+                <td><input type='text' id='pseudo' name='pseudo' required></td>
+        </tr>
+        <tr>
+                <td><label for='password'>Password:</label></td>
+                <td><input type='password' id='password' name='password' required></td>
+        </tr>
+        </table>
+        <button type='submit'>Log in</button><button type='submit'>Log Out</button>
 </form>
+</div>
 <a href="index.php?q=session&categ=user&sub=add">Créer un compte</a>
+</div>
+</center>
+</body>
+</html>
