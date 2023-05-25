56e90916b5bef634f52a97e9cb144d1b5cbd172b
David <davidnjandjeu@gmail.com>
2023-05-25 06:14:32 +0100

    Update deleteclass.inc.php

--- views/tools/classificationScheme/deleteclass.inc.php
+++ views/tools/classificationScheme/deleteclass.inc.php
@@ -5,6 +5,7 @@ require_once 'models/tools/classification/class.class.php';
 require_once 'models/tools/classification/classesManager.class.php';
 $class = new activityClasse();
 $class ->setClassById($_GET['id']);
+$classmanager = new activityClassesManager();
 if ($class->numberChildUsed() > 0) {
     echo '<h1>Vous ne pouvez pas supprimer cette classe qui contient ' . $class->numberChildUsed() . ' sous classe</h1>';
 }
@@ -16,12 +17,13 @@ else{
     echo $class->getClassTitle() . "  ";
     echo $class ->getClassObservation();
     echo $class ->numberChildUsed()." sous-classes". "  ";
+    $$classmanager  ->deleteClassificationById($_GET['id']);
 }
 
 
 echo "<hr/>";
+deleteClassificationById
 
-$classChild = new activityClassesManager();
 
 ?>
 <a href="index.php?q=tools&categ=classificationScheme&sub=mainClasses">Classes principales</a><br/>
