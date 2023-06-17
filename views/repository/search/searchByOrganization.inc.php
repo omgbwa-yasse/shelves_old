9bc0ac972ee6e760b5079cfda580866c0f5341a5
omgbwa yasse <omgbwayasse@gmail.com>
2023-06-17 21:02:41 +0100

    modified:   models/tools/organization/organizationManager.class.php 	modified:   views/repository/search/searchByOrganization.inc.php

--- views/repository/search/searchByOrganization.inc.php
+++ views/repository/search/searchByOrganization.inc.php
@@ -1,11 +1,17 @@
 <?php
 require_once 'models/tools/organization/organizationManager.class.php';
+require_once 'models/tools/organization/organization.class.php';
+
 $allOrganization = new organizationManager();
 $list = $allOrganization -> getAllOrganization();
 echo "<div style=\"margin:30px 0px 0px 30px;padding:20px 20px 20px 20px;;border:solid 2px red;width:900px\">";
-foreach($list as $organization){
+foreach($list as $id){
+    $organization = new organization();
+    $organization ->setOrganizationById($id['organization_id']);
+    
     echo "<a href=\"index.php?q=repository&categ=search&sub=organization&id="
-    . $organization['organization_id'] ."\">" . $organization['organization_code'] . " - " . $organization['organization_title'] . "</a><br>";
+    . $organization->getOrganizationId() ."\">" . $organization->getOrganizationCode() . " - " . $organization->getOrganizationTitle() . "</a><br>";
+        
     }
 echo "</div>";
 ?>
