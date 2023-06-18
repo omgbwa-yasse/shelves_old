18117458a2708351949d311c589d546b3ec33a3f
David <davidnjandjeu@gmail.com>
2023-06-18 18:13:00 +0100

    organigramme

---
+++ models/tools/organization/organigram.js
@@ -0,0 +1,16 @@
+var Diagram = MindFusion.Diagramming.Diagram;
+var CompositeNode = MindFusion.Diagramming.CompositeNode;
+var Behavior = MindFusion.Diagramming.Behavior;
+var Events = MindFusion.Diagramming.Events;
+var Theme = MindFusion.Diagramming.Theme;
+var Style = MindFusion.Diagramming.Style;
+
+var Alignment = MindFusion.Drawing.Alignment;
+var Rect = MindFusion.Drawing.Rect;
+var Point = MindFusion.Drawing.Point;
+
+var TreeLayout = MindFusion.Graphs.TreeLayout;
+
+var diagram = null;
+var names;
+var coloredNode;
