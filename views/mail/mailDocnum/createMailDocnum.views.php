4481f104e57bc63bdc9daeb612b07efdaef3efe2
Diandreas <davidnjandjeu@gmail.com>
2024-03-31 17:22:15 +0100

    Update createMailDocnum.views.php
    Creation des document numerique

--- views/mail/mailDocnum/createMailDocnum.views.php
+++ views/mail/mailDocnum/createMailDocnum.views.php
@@ -3,22 +3,37 @@ require_once 'models/mail/mailDocNum.class.php';
 
 $mailDocNum = new MailDocNum();
 
-if (isset($_POST['mail_docnum_path']) && isset($_POST['mail_docnum_filename'])) {
- 
-    $mailDocNum -> createMailDocNum(NULL,$_POST['mail_docnum_path'], $_POST['mail_docnum_filename']);
+if (isset($_FILES['mail_docnum_file'])) {
+    $originalName = $_FILES['mail_docnum_file']['name'];
+    $tempname = $_FILES['mail_docnum_file']['tmp_name'];    
+    
+    // Créer une arborescence de dossiers basée sur la date et l'heure
+    $folder = "Document/" . date('Y/m/d/') ;
+    
+    if (!file_exists($folder)) {
+        mkdir($folder, 0777, true);
+    }
+    $heure=date('H-i-');
+    echo $heure;
+    $filename =$heure.$originalName;
+    $newFilePath = $folder . $filename;
+    
+    if (move_uploaded_file($tempname, $newFilePath)) {
+        $mailDocNum -> createMailDocNum(NULL, $newFilePath, $filename);
+    } else {
+        echo "Failed to upload file.";
+    }
 }
 ?>
-<h1>Créer un  Couriels DocNum </h1>
 
-<form  method="POST" action="index.php?q=mail&categ=mailDocNum&sub=createMailDocNum">
+<h1>Créer un Couriels DocNum </h1>
+
+<form method="POST" action="index.php?q=mail&categ=mailDocnum&sub=create" enctype="multipart/form-data">
 <table>
   <tr>
-    <td><label for="mail_docnum_path">Chemin du  Couriels DocNum :</label></td>
-    <td><input type="text" id="mail_docnum_path" name="mail_docnum_path"></td>
+    <td><label for="mail_docnum_file">Fichier du Couriels DocNum :</label></td>
+    <td><input type="file" id="mail_docnum_file" name="mail_docnum_file" accept=".pdf"></td>
   </tr>
-  <tr>
-    <td><label for="mail_docnum_filename">Nom de fichier du  Couriels DocNum :</label></td>
-    <td><input type="text" id="mail_docnum_filename" name="mail_docnum_filename"></td>
-  </tr>
- <tr><td><input type="submit" value="Submit"></td>   <td><input type="reset" value="Annuler"></td></tr> </table>
+  <tr><td><input type="submit" value="Submit"></td>   <td><input type="reset" value="Annuler"></td></tr> 
+</table>
 </form>
