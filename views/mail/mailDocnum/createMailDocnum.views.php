8e46d9b61a3ac305abe3ac2fd35dc5fb526c8b00
Diandreas <davidnjandjeu@gmail.com>
2024-03-31 17:27:08 +0100

    sweetallert

--- views/mail/mailDocnum/createMailDocnum.views.php
+++ views/mail/mailDocnum/createMailDocnum.views.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'models/mail/mailDocNum.class.php';
-
 $mailDocNum = new MailDocNum();
+$mailDocNumAdded = false;
 
 if (isset($_FILES['mail_docnum_file'])) {
     $originalName = $_FILES['mail_docnum_file']['name'];
@@ -14,26 +14,55 @@ if (isset($_FILES['mail_docnum_file'])) {
         mkdir($folder, 0777, true);
     }
     $heure=date('H-i-');
-    echo $heure;
+ 
     $filename =$heure.$originalName;
     $newFilePath = $folder . $filename;
     
     if (move_uploaded_file($tempname, $newFilePath)) {
         $mailDocNum -> createMailDocNum(NULL, $newFilePath, $filename);
+        $mailDocNumAdded = true;
     } else {
         echo "Failed to upload file.";
     }
 }
 ?>
 
-<h1>Créer un Couriels DocNum </h1>
+<!DOCTYPE html>
+<html lang="en">
 
-<form method="POST" action="index.php?q=mail&categ=mailDocnum&sub=create" enctype="multipart/form-data">
-<table>
-  <tr>
-    <td><label for="mail_docnum_file">Fichier du Couriels DocNum :</label></td>
-    <td><input type="file" id="mail_docnum_file" name="mail_docnum_file" accept=".pdf"></td>
-  </tr>
-  <tr><td><input type="submit" value="Submit"></td>   <td><input type="reset" value="Annuler"></td></tr> 
-</table>
-</form>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        /* Votre code CSS ici */
+    </style>
+    <!-- Inclure la bibliothèque SweetAlert -->
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+</head>
+
+<body>
+    <h1>Créer un Couriels DocNum </h1>
+    <form method="POST" action="index.php?q=mail&categ=mailDocnum&sub=create" enctype="multipart/form-data" id="myForm">
+    <table>
+      <tr>
+        <td><label for="mail_docnum_file">Fichier du Couriels DocNum :</label></td>
+        <td><input type="file" id="mail_docnum_file" name="mail_docnum_file" accept=".pdf"></td>
+      </tr>
+      <tr><td><input type="submit" value="Submit"></td>   <td><input type="reset" value="Annuler"></td></tr> 
+    </table>
+        
+        <!-- Champ de formulaire caché pour l'indicateur de Couriels DocNum ajouté -->
+        <input type="hidden" id="mailDocNumAdded" name="mailDocNumAdded" value="<?php echo $mailDocNumAdded ? 'true' : 'false'; ?>">
+    </form>
+    <script>
+        window.addEventListener('load', function() {
+            var mailDocNumAdded = document.getElementById('mailDocNumAdded').value;
+            if (mailDocNumAdded === 'true') {
+                swal("Good job!", "mail DocNum added successfully!", "success");
+            }
+        });
+    </script>
+</body>
+
+</html>
